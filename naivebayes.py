69ee4b1c8d3e0c9dc2a433fe33a561d58c45ec37
ryant <ryanlui.college@gmail.com>
2019-11-17 15:35:34 -0800

    Refactor naivebayes to have a Learner class

--- naivebayes.py
+++ naivebayes.py
@@ -1,51 +1,66 @@
 """Naive Bayes implementation"""
 import csv
 
-def read_csv(filename):
-    """
-    Reads data in from csv file
-    :param filename: path to csv file as a string
-    :return: list of data read from csv file
-    """
-    with open(filename) as file:
-        reader = csv.reader(file, delimiter=',')
-        dataset = list()
-    return dataset
 
-def separate_by_class(dataset):
+class Learner:
     """
-    https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
-    Separates dataset into its given classes. In this case, there are only two classes: normal and abnormal.
-    :param dataset: Data to be separated into classes
-    :return: dictionary of lists where the key is the class and value is a list of vectors in that class.
+    Contains information on the dataset, performs training, and classifies.
     """
-    classes = dict()
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        class_value = vector[0]
-        if class_value not in classes:
-            classes[class_value] = list()
-        classes[class_value].append(vector)
-    return classes
 
-def train(training_set):
-    """
-    Train learner via training_set
-    :param training_set: List of training instances t has a class t.c and array of features t.f
-    :return:feature_class: 2-dimensional array of counts feature_class[i,j] where left dimen is
-        an instance classification (0 or 1) and right dimension is a feature number.
-        Contents of each array entry is a count of the number of times
-        that the given feature appears positive in the given class.
-    :return:classes: Array classes[i] indexed by instance classification and gives the count of training instances with
-        that class.
-    """
-    feature_class = [[0 for i in range(22)] for i in range(2)]
-    classes = [0, 0]
-    for t in training_set:
-        classes[t[0]] += 1
-        j = 0
-        for feature in t[1:]:
-            if feature == 1:
-                feature_class[t[0]][j] += 1
-            j += 1
-    return feature_class, classes
+    def __init__(self, num_features=0, num_classes=0):
+        self.num_features = num_features
+        self.num_classes = num_classes
+        self.feature_class = []
+        self.classes = []
+
+    def read_csv(self, filename):
+        """
+        Reads data in from csv file
+        :param filename: path to csv file as a string
+        :return: list of data read from csv file
+        """
+        with open(filename) as file:
+            reader = csv.reader(file, delimiter=',')
+            dataset = list()
+        return dataset
+
+    def separate_by_class(self, dataset):
+        """
+        https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+        Separates dataset into its given classes. In this case, there are only two classes: normal and abnormal.
+        :param dataset: Data to be separated into classes
+        :return: dictionary of lists where the key is the class and value is a list of vectors in that class.
+        """
+        classes = dict()
+        for i in range(len(dataset)):
+            vector = dataset[i]
+            class_value = vector[0]
+            if class_value not in classes:
+                classes[class_value] = list()
+            classes[class_value].append(vector)
+        return classes
+
+    def train(self, training_set):
+        """
+        Train learner via training_set
+        :param training_set: List of training instances t has a class t.c and array of features t.f
+        :return:feature_class: 2-dimensional array of counts feature_class[i,j] where left dimen is
+            an instance classification (0 or 1) and right dimension is a feature number.
+            Contents of each array entry is a count of the number of times
+            that the given feature appears positive in the given class.
+        :return:classes: Array classes[i] indexed by instance classification and gives the count of training instances with
+            that class.
+        """
+        feature_class = [[0 for i in range(self.num_features)] for i in range(self.num_classes)]
+        classes = [0, 0]
+        for t in training_set:
+            classes[t[0]] += 1
+            j = 0
+            for feature in t[1:]:
+                if feature == 1:
+                    feature_class[t[0]][j] += 1
+                j += 1
+        return feature_class, classes
+
+    def classify(self):
+        return 0
--- test_naivebayes.py
+++ test_naivebayes.py
@@ -1,8 +1,8 @@
 import unittest
-import naivebayes
+from naivebayes import Learner
 
 default_dataset = [
-    # 23 numbers, 22 features, 1st is the class
+    # 23 numbers, 22 features, 1st is the class (1 of 2)
     [1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
@@ -26,11 +26,14 @@ default_feature_class = [
 
 class TestNaivebayes(unittest.TestCase):
     def test_separate_by_class(self):
-        classes = naivebayes.separate_by_class(default_dataset)
+        nb = Learner(22, 2)
+        classes = nb.separate_by_class(default_dataset)
         self.assertEqual(default_dict, classes)
 
     def test_train(self):
-        feature_class, classes = naivebayes.train(default_dataset)
+        nb = Learner(22, 2)
+        feature_class, classes = nb.train(default_dataset)
         self.assertEqual(default_feature_class, feature_class)
         self.assertEqual(classes[0], 2)
         self.assertEqual(classes[1], 2)
+
