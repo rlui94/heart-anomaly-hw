f710274c8f93c57306006b71de16cb58923a58f5
ryant <ryanlui.college@gmail.com>
2019-11-18 12:31:06 -0800

    Imlemented test for distance method

---
+++ test_knn.py
@@ -0,0 +1,25 @@
+import unittest
+from knn import Learner
+
+default_dataset = [
+    # 23 numbers, 22 features, 1st is the class (1 of 2)
+    [1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+small_set = [
+    [1, 4, 1],
+    [2, 8, 4]
+]
+
+
+class TestKnn(unittest.TestCase):
+    def test_distance(self):
+        kn = Learner()
+        self.assertEqual(5, kn.distance(small_set[0], small_set[1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
